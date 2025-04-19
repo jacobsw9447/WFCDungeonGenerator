64a09fd32755c32098243c1e676a4d116eaa84c5
Staton Otto <98541089+GammaVolantis@users.noreply.github.com>
2025-04-19 15:11:45 -0500

    Basic pygame functionality completed

---
+++ Core.py
@@ -0,0 +1,61 @@
+#Core Code for data viewing and tile setup/import.
+# [ ] Code to handle importing of image tiles and display into pygame
+# [ ] Code to handle displaying this data
+# [ ] Code to handle moving around tilespace
+# [ ] Draw method to draw on screen with the array return from Generator.py
+# [ ] Update method to update screen with potential updates from the generator file or other code
+
+# [ ] Code to make me a sandwich (OPTIONAL BUT HIGHLY RECOMMENDED)
+
+import pygame
+from pygame.math import Vector2
+from pygame.locals import *
+import math
+
+
+#UPDATE METHOD
+def update(dt):
+    pass
+
+
+#DRAW METHOD
+def draw(window):
+    pass
+
+
+
+
+#-------------------------------START OF GAMELOOP CODE-----------------------------------------------------------
+pygame.init()
+width, height = 1000,800
+window = pygame.display.set_mode([width, height])
+font = pygame.font.SysFont('impact', 30, False, False)
+#STARTING VARIABLES
+state = "run"
+data = [[],[],[],[]]
+map = []
+
+# timing
+fps = 60
+dt = 1/fps
+clock = pygame.time.Clock()
+
+#RUN LOOP
+while state != "quit":
+    pygame.display.update()
+    clock.tick(fps)
+    #clears screen with a black background
+    window.fill([0,0,0])
+
+    #EVENTS
+    key = pygame.key.get_pressed()
+    while event := pygame.event.poll():
+        if event.type == pygame.QUIT or event.type == KEYDOWN and event.key == K_ESCAPE:
+            state = "quit"
+
+    update(dt)
+    draw(window)
+
+
+    
+
---
+++ Generator.py
@@ -0,0 +1,18 @@
+# Generator Code for the Wave Function Collapse Algorithm
+# [ ] Proccessing Method to take in an array of data and set up the rotations and new array of tiled data
+# [ ] Generation Method that takes the above array and a map array to fill and processes the tiles into the array for viewing
+# [ ] ADD - Generation Method needs to handle backtracking
+
+
+import pygame
+from pygame.math import Vector2
+from pygame.locals import *
+import math
+
+
+def Proccessing(data):
+    pass
+
+
+def Generation(data, map):
+    pass
